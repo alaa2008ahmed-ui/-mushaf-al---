8ef182f766fab6b996710897bfdadc470840078e
Alaa Ahmed <alaa2008ahmed@gmail.com>
2026-02-23 10:51:02 +0300

    Update: Mon 02/23/2026 10:51:02.64

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.AhmedLaila.Quran',
-  appName: 'مصحف أحمد وليلى',
-  webDir: 'dist'
-};
-
-export default config;
