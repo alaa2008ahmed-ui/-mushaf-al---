4b985823ba0c8dd655927ddd0825990497646208
Alaa Ahmed <alaa2008ahmed@gmail.com>
2026-02-27 15:22:05 +0300

    Remove splash screen and set theme to NoActionBar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,15 +5,11 @@ const config: CapacitorConfig = {
   appName: 'remix-mushaf-alaa',
   webDir: 'dist',
   bundledWebRuntime: false,
-  // إضافة قسم الإضافات (Plugins) للتحكم في شاشة البداية
   plugins: {
     SplashScreen: {
-      launchShowDuration: 5000, // المدة 3 ثوانٍ (يمكنك تغييرها من هنا)
-      launchAutoHide: true,     // تختفي تلقائياً بعد انتهاء الوقت
-      launchFadeOutDuration: 500, // مدة التلاشي عند الاختفاء (نصف ثانية)
-      backgroundColor: "#ffffff", // لون الخلفية (يجب أن يطابق ما وضعته في Styles)
-      androidScaleType: "CENTER_CROP", // لجعل الصورة تغطي المساحة بشكل جيد
-      showSpinner: false        // هل تريد إظهار دائرة تحميل فوق الصورة؟ (false للإخفاء)
+      launchShowDuration: 0,     // جعل مدة العرض صفر
+      launchAutoHide: true,      // إخفاء تلقائي فوري
+      backgroundColor: "#ffffff" // لون خلفية احتياطي سريع جداً
     }
   }
 };
