016dbe144f8f316f41cafe213eb6dc2cc4e22257
Alaa Ahmed <alaa2008ahmed@gmail.com>
2026-02-27 20:32:11 +0300

    (Fri 02/27/2026 20:32:11.18)

--- android/app/src/main/java/com/Ahmed11Laila/Quran/MainActivity.java
+++ android/app/src/main/java/com/Ahmed11Laila/Quran/MainActivity.java
@@ -1,16 +1,5 @@
 package com.Ahmed11Laila.Quran;
 
-import android.os.Bundle;
-import android.graphics.Color;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // منع الوميض الأبيض بجعل خلفية الـ WebView شفافة برمجياً
-        // هذا السطر يضمن أنه حتى لو تأخر التطبيق في التحميل، لن يظهر لون أبيض
-        this.bridge.getWebView().setBackgroundColor(Color.TRANSPARENT);
-    }
-}
+public class MainActivity extends BridgeActivity {}
